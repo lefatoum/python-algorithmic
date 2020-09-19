eede2260c63807e48c9da3ade2f3705b70878394
Chris <lefatoum@hotmail.fr>
2020-09-19 03:13:11 +0200

    First

---
+++ dayoftheweek.py
@@ -0,0 +1,4 @@
+dw ={'lundi': 1, 'mardi': 2, 'mercredi': 3, 'jeudi': 4, 'vendredi': 5, 'samedi': 6, 'dimanche': 7}
+print(dw['lundi'])
+dw2 = {1: 'lundi', 2: 'mardi', 3: 'mercredi', 4: 'jeudi', 5: 'vendredi', 6: 'samedi', 7: 'dimanche'}
+print(dw2[1])
